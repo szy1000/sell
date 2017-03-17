939a64e693f27d0443e225f5a6faffeafb48665a
szy1000 <916698462@qq.com>
2017-03-18 00:18:01 +0800

    goods-list

--- ele/src/components/goods/goods.vue
+++ ele/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper">
+		<div class="menu-wrapper" ref:menu-wrapper>
 			<ul>
 				<li v-for="item in goods" class="menu-item">
 					<span class="text">
@@ -10,13 +10,37 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper">
-			
+		<div class="foods-wrapper" ref:food-wrapper>
+			<ul>
+				<li v-for="item in goods" class="food-list">
+					<h1 class="title">{{item.name}}</h1>
+					<ul>
+						<li v-for="food in item.foods" class="food-item">
+							<div class="icon">
+								<img width="57" height="57" :src="food.icon" alt="">
+							</div>
+							<div class="content">
+								<h2 class="name">{{food.name}}</h2>
+								<p class="desc">{{food.description}}</p>
+								<div class="extra">
+									<span class="count">月售{{food.sellCount}}份</span>
+									<span>好评率{{food.rating}}%</span>
+								</div>
+								<div class="price">
+									<span class="now">￥{{food.price}}</span>
+									<span v-show="food.oldPrice" class="oldPrice">￥{{food.oldPrice}}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
+	import BScroll from 'better-scroll'
 	const ERR_OK = 0;
 
 	export default {
@@ -37,8 +61,18 @@
 				response = response.body;
 				if(response.errno == ERR_OK){
 					this.goods = response.data;
+					this.$nextTick(()=>{
+						this._initScroll();
+					})
 				}
 			})
+		},
+		methods: {
+			_initScroll(){
+				this.menuScroll = new BScroll(this.$el.menuWrapper,{});
+
+				this.foodScroll = new BScroll(this.$el.foodWrapper,{})
+			}
 		}
 	}
 </script>
@@ -97,7 +131,68 @@
 		}
 
 		.foods-wrapper{
-
+			flex:1;
+			.title{
+				padding-left:14px;
+				height:26px;
+				line-height:26px;
+				border-left:2px solid #d9dde1;
+				font-size:12px;
+				color:rgb(147,153,159);
+				background-color: #f3f5f7;
+			}
+			.food-item{
+				display:flex;
+				margin:18px;
+				padding-bottom:18px;
+				@include border-1px(rgba(7,17,27,.1));
+				&:last-child{
+					@include border-none(rgba(7,17,27,.1))
+					padding-bottom:0;
+				}
+				.icon{
+					flex: 0 0 57px;
+					margin-right: 10px;
+				}
+				.content{
+					flex: 1;
+					.name{
+						margin: 2px 0 8px 0;
+						height:14px;
+						font-size: 14px;
+						line-height:14px;
+						color:rgb(7,17,27);
+					}
+					.desc,.extra{
+						line-height: 10px;
+						font-size: 10px;
+						color:rgb(147, 153, 159);
+					}
+					.desc{
+						margin-bottom: 8px;
+					}
+					.extra{
+						line-height:10px;
+						.count{
+							margin-right:12px;
+						}
+					}
+					.price{
+						font-weight: 700;
+						line-height: 24px;
+						.now{
+							margin-right: 8px;
+							font-size: 14px;
+							color:rgb(240,20,20);
+						}
+						.oldPrice{
+							font-size: 10px;
+							color:rgb(147, 153, 159);
+							text-decoration: line-through;
+						}
+					}
+				}
+			}
 		}
 	}
 </style>
