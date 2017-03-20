1c12e34ca43b43c0ea04db322fa685f7eec1fee9
szy1000 <916698462@qq.com>
2017-03-20 22:04:19 +0800

    add goods

--- ele/src/components/goods/goods.vue
+++ ele/src/components/goods/goods.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper" ref:menu-wrapper>
+		<div class="menu-wrapper" ref="menuWrapper">
 			<ul>
-				<li v-for="item in goods" class="menu-item">
+				<li v-for="(item,index) in goods" class="menu-item" :class="{ 'current': currentIndex === index}">
 					<span class="text">
 						<span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
 						{{item.name}}
@@ -10,9 +10,9 @@
 				</li>
 			</ul>
 		</div>
-		<div class="foods-wrapper" ref:food-wrapper>
+		<div class="foods-wrapper" ref="foodWrapper">
 			<ul>
-				<li v-for="item in goods" class="food-list">
+				<li v-for="item in goods" class="food-list food-list-hook">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
 						<li v-for="food in item.foods" class="food-item">
@@ -51,7 +51,21 @@
 		},
 		data(){
 			return {
-				goods:[]
+				goods:[],
+				listHeight:[],
+				scrollY: 0
+			}
+		},
+		computed:{
+			currentIndex(){
+				for(let i=0 ;i<this.listHeight.length;i++){
+					let height1 = this.listHeight[i];
+					let height2 = this.listHeight[i+1];
+					if(!height2 || (this.scrollY >= height1 && this.scroll < height2)){
+						return i;
+					}
+				}
+				return 0;
 			}
 		},
 		created(){
@@ -61,17 +75,33 @@
 				response = response.body;
 				if(response.errno == ERR_OK){
 					this.goods = response.data;
-					this.$nextTick(()=>{
+					this.$nextTick(() => {
 						this._initScroll();
+						this._calculateHeight();
 					})
 				}
 			})
 		},
 		methods: {
 			_initScroll(){
-				this.menuScroll = new BScroll(this.$el.menuWrapper,{});
+				this.menuScroll = new BScroll(this.$refs.menuWrapper,{});
 
-				this.foodScroll = new BScroll(this.$el.foodWrapper,{})
+				this.foodScroll = new BScroll(this.$refs.foodWrapper,{
+					probeType: 3
+				});
+				this.foodScroll.on('scroll', (pos) => {
+					this.scrollY = Math.abs(Math.round(pos.y));
+				})
+			},
+			_calculateHeight(){
+				let foodList = this.$refs.foodWrapper.getElementsByClassName("food-list-hook");
+				let height = 0;
+				this.listHeight.push(height);
+				for(let i=0 ; i<foodList.length;i++){
+					let item = foodList[i];
+					height+=item.clientHeight;
+					this.listHeight.push(height);
+				}
 			}
 		}
 	}
@@ -98,6 +128,16 @@
 				width:56px;
 				line-height: 14px;
 				@include border-1px(rgba(7,17,27,.1));
+				&.current{
+					position: relative;
+					z-index: 10;
+					margin-top: -1px;
+					background: #fff;
+					font-weight: 700;
+					.text{
+						@include border-none(rgba(7,17,27,.1));
+					}
+				}
 				.icon{
 					display:inline-block;
 					margin-right:2px;
